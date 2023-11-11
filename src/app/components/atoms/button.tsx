e6ebdd755caa6a8d4c145d69b1da2c9bc6fb75b3
Khalid Sulyman <84248666+Khaalid-oh@users.noreply.github.com>
2023-11-11 22:21:11 +0100

    Update button.tsx

--- src/app/components/atoms/button.tsx
+++ src/app/components/atoms/button.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Button() {
   return (
     <button type='submit'  className='bg-blue-950 h-[38px] px-3 py-2 text-white text-sm rounded-md'>
-      Apply @
+      Apply lop
     </button>
   )
 }
